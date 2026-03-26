cfc7959b906ebf6a8455d546c4bb0cb0aeea94be
Fry Bansee <121478476+Fry-Bansee139@users.noreply.github.com>
2026-03-26 13:13:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  siteName: "sawahdb",
-  authorName: "sawahdb",
+  siteName: "SawahDatabase",
+  authorName: "SawahDatabase",
   profilePicture: "https://cdn.nefyu.my.id/csy1.jpg",
-  profileBanner: "https://cdn.nefyu.my.id/cgxl.png",
-  aboutText: "A personal project for file hosting and URL shortening, built with simplicity and efficiency in mind.",
+  profileBanner: "https://sawahdb.vercel.app/IEvk.jpg",
+  aboutText: "Sebuah project privat untuk hosting file dan short URL, dibangun dengan mengutamakan responsif dan penyimpanan.",
   backgroundImage: "https://cdn.nefyu.my.id/1lsq.webp"
 };
